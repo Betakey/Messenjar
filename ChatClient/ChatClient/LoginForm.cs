635c1412a507aacf7f210d007a5a4ca7da8183f1
Alexander Zheng <1999alex1@web.de>
2018-02-06 17:26:49 +0100

    Login Function

--- ChatClient/ChatClient/LoginForm.cs
+++ ChatClient/ChatClient/LoginForm.cs
@@ -8,13 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ChatClient
 {
     public partial class LoginForm : Form
     {
+        Database db;
         public LoginForm()
         {
             InitializeComponent();
+            db = new Database();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            db.Login(usernameTextBox.Text, passwordTextBox.Text);
         }
     }
 }
